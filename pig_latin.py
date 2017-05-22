12c89bf90be2fb095be3b61f5db4fe62809bddb5
Amelia Green <ameliacgreen@gmail.com>
2017-05-22 14:55:16 -0400

    Finished pig latin

---
+++ pig_latin.py
@@ -0,0 +1,28 @@
+def pig_latin(phrase):
+    """Turn a phrase into pig latin.
+
+    There will be no uppercase letters or punctuation in the phrase.
+
+        >>> pig_latin('hello awesome programmer')
+        'ellohay awesomeyay rogrammerpay'
+    """
+
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+
+    words = phrase.split()
+    print words
+
+    pig_words = []
+
+    for word in words:
+        if word[0].lower() in vowels:
+            pig_word = word + 'yay'
+            print pig_word
+            pig_words.append(pig_word)
+        else:
+            pig_word = word[1:] + word[0] + 'ay'
+            pig_words.append(pig_word)
+
+    return pig_words
+
+print pig_latin("hello awesome programmer")
